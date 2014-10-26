796bffd7932ab2fc0b5ee418e38a9664e601ec66
namwoody <deepsarora262@gmail.com>
2014-10-26 10:59:42 +0530

    changed the path in config file

--- config.php
+++ config.php
@@ -6,16 +6,16 @@ define('HTTP_SERVER', 'http://kartzill.com/');
 define('HTTPS_SERVER', 'http://kartzill.com/');
 
 // DIR
-define('DIR_APPLICATION', '/public/kart-zill/catalog/');
-define('DIR_SYSTEM', '/public/kart-zill/system/');
-define('DIR_DATABASE', '/public/kart-zill/system/database/');
-define('DIR_LANGUAGE', '/public/kart-zill/catalog/language/');
-define('DIR_TEMPLATE', '/public/kart-zill/catalog/view/theme/');
-define('DIR_CONFIG', '/public/kart-zill/system/config/');
-define('DIR_IMAGE', '/public/kart-zill/image/');
-define('DIR_CACHE', '/public/kart-zill/system/cache/');
-define('DIR_DOWNLOAD', '/public/kart-zill/download/');
-define('DIR_LOGS', '/public/kart-zill/system/logs/');
+define('DIR_APPLICATION', '/home/forge/kartzill.com/public/kart-zill/catalog/');
+define('DIR_SYSTEM', '/home/forge/kartzill.com/public/kart-zill/system/');
+define('DIR_DATABASE', '/home/forge/kartzill.com/public/kart-zill/system/database/');
+define('DIR_LANGUAGE', '/home/forge/kartzill.com/public/kart-zill/catalog/language/');
+define('DIR_TEMPLATE', '/home/forge/kartzill.com/public/kart-zill/catalog/view/theme/');
+define('DIR_CONFIG', '/home/forge/kartzill.com/public/kart-zill/system/config/');
+define('DIR_IMAGE', '/home/forge/kartzill.com/public/kart-zill/image/');
+define('DIR_CACHE', '/home/forge/kartzill.com/public/kart-zill/system/cache/');
+define('DIR_DOWNLOAD', '/home/forge/kartzill.com/public/kart-zill/download/');
+define('DIR_LOGS', '/home/forge/kartzill.com/public/kart-zill/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
