100a4eeeaf296d28c8a1c58aa6596e20ac4192c7
namwoody <deepsarora262@gmail.com>
2014-10-26 10:40:55 +0530

    edit config file

--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8000/');
+define('HTTP_SERVER', 'http://kartzill.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8000/');
+define('HTTPS_SERVER', 'http://kartzill.com/');
 
 // DIR
-define('DIR_APPLICATION', '/Users/unobab/Desktop/kitchen/catalog/');
-define('DIR_SYSTEM', '/Users/unobab/Desktop/kitchen/system/');
-define('DIR_DATABASE', '/Users/unobab/Desktop/kitchen/system/database/');
-define('DIR_LANGUAGE', '/Users/unobab/Desktop/kitchen/catalog/language/');
-define('DIR_TEMPLATE', '/Users/unobab/Desktop/kitchen/catalog/view/theme/');
-define('DIR_CONFIG', '/Users/unobab/Desktop/kitchen/system/config/');
-define('DIR_IMAGE', '/Users/unobab/Desktop/kitchen/image/');
-define('DIR_CACHE', '/Users/unobab/Desktop/kitchen/system/cache/');
-define('DIR_DOWNLOAD', '/Users/unobab/Desktop/kitchen/download/');
-define('DIR_LOGS', '/Users/unobab/Desktop/kitchen/system/logs/');
+define('DIR_APPLICATION', '/public/kart-zill/catalog/');
+define('DIR_SYSTEM', '/public/kart-zill/system/');
+define('DIR_DATABASE', '/public/kart-zill/system/database/');
+define('DIR_LANGUAGE', '/public/kart-zill/catalog/language/');
+define('DIR_TEMPLATE', '/public/kart-zill/catalog/view/theme/');
+define('DIR_CONFIG', '/public/kart-zill/system/config/');
+define('DIR_IMAGE', '/public/kart-zill/image/');
+define('DIR_CACHE', '/public/kart-zill/system/cache/');
+define('DIR_DOWNLOAD', '/public/kart-zill/download/');
+define('DIR_LOGS', '/public/kart-zill/system/logs/');
 
 // DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'kitchen');
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'forge');
+define('DB_PASSWORD', 'vNgnyVEKVFShH0hzz6uT');
+define('DB_DATABASE', 'kartzill');
 define('DB_PREFIX', 'oc_');
 ?>
