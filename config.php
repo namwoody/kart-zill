b1794be0f4613c7999694af04b7842bf1f293477
namwoody <deepsarora262@gmail.com>
2014-10-26 08:52:20 +0530

    changed a config file

--- config.php
+++ config.php
@@ -19,9 +19,9 @@ define('DIR_LOGS', '/Users/unobab/Desktop/kitchen/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'kitchen');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'forge');
+define('DB_PASSWORD', 'vNgnyVEKVFShH0hzz6uT');
+define('DB_DATABASE', 'kartzill');
 define('DB_PREFIX', 'oc_');
 ?>
